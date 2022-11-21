30f522e01015d1dddbba316dea366f5c9c240637
polson <polson.k@gmail.com>
2022-11-21 11:19:44 +0700

    wip read Huawei Health data. Still auth issue

--- app/src/main/java/com/anotherdev/superhealth/MainActivity.kt
+++ app/src/main/java/com/anotherdev/superhealth/MainActivity.kt
@@ -6,18 +6,25 @@ import android.view.MenuItem
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
 import com.anotherdev.superhealth.databinding.ActivityMainBinding
+import com.huawei.hmf.tasks.OnSuccessListener
 import com.huawei.hms.hihealth.ConsentsController
+import com.huawei.hms.hihealth.DataController
 import com.huawei.hms.hihealth.HuaweiHiHealth
 import com.huawei.hms.hihealth.SettingController
+import com.huawei.hms.hihealth.data.DataType
+import com.huawei.hms.hihealth.data.SamplePoint
 import com.huawei.hms.hihealth.data.Scopes
+import com.huawei.hms.hihealth.options.ReadOptions
 import timber.log.Timber
+import java.time.ZonedDateTime
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     private val scopes = arrayOf(
         Scopes.HEALTHKIT_STEP_READ,
-        //Scopes.HEALTHKIT_HEIGHTWEIGHT_READ,
-        //Scopes.HEALTHKIT_HEARTRATE_READ,
+        Scopes.HEALTHKIT_HEARTRATE_READ,
+        Scopes.HEALTHKIT_HEIGHTWEIGHT_READ,
         //Scopes.HEALTHKIT_ACTIVITY_READ,
         //Scopes.HEALTHKIT_ACTIVITY_RECORD_READ,
     )
@@ -25,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var healthSettingController: SettingController
     private lateinit var consentsController: ConsentsController
+    private lateinit var dataController: DataController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,6 +61,7 @@ class MainActivity : AppCompatActivity() {
                 requestAuthHuaweiHealth()
             }
             R.id.menu_connect -> {
+                read()
             }
         }
         return super.onOptionsItemSelected(item)
@@ -61,6 +70,7 @@ class MainActivity : AppCompatActivity() {
     private fun initHuaweiHealth() {
         healthSettingController = HuaweiHiHealth.getSettingController(this)
         consentsController = HuaweiHiHealth.getConsentsController(this)
+        dataController = HuaweiHiHealth.getDataController(this)
 
         val scope = scopes[0]
         consentsController.get(scope, "en-us")
@@ -83,5 +93,42 @@ class MainActivity : AppCompatActivity() {
         } else {
             Timber.e("Huawei Health Request Auth failed. authResult: %s", authResult)
         }
+    }
+
+    private fun read() {
+        val onSuccess = OnSuccessListener<Map<DataType,SamplePoint>> { samplePointMap ->
+            samplePointMap?.forEach { entry ->
+                Timber.e("Type: %s SamplePoint: %s", entry.key, entry.value)
+                entry.value.fieldValues.forEach {
+                    Timber.e("  Field name: %s value: %s", it.key, it.value)
+                }
+            }
+        }
+
+        val dataTypes = listOf(
+            DataType.DT_INSTANTANEOUS_HEART_RATE
+        )
+        dataController.readLatestData(dataTypes)
+            .addOnSuccessListener(onSuccess)
+            .addOnFailureListener { Timber.e(it, "readLatestData error %s", dataTypes) }
+
+        val yesterday = ZonedDateTime.now().toInstant().toEpochMilli()
+        val now = ZonedDateTime.now().toInstant().toEpochMilli()
+        val readOptions = ReadOptions.Builder()
+            .read(DataType.DT_CONTINUOUS_STEPS_TOTAL)
+            .setTimeRange(yesterday, now, TimeUnit.MILLISECONDS)
+            .build()
+        dataController.read(readOptions)
+            .addOnSuccessListener { readReply ->
+                readReply.sampleSets.flatMap { it.samplePoints }
+                    .forEach { samplePoint ->
+                        samplePoint.toString()
+                        Timber.e("SamplePoint: %s", samplePoint)
+                        samplePoint.fieldValues.forEach {
+                            Timber.e("  Field name: %s value: %s", it.key, it.value)
+                        }
+                    }
+            }
+            .addOnFailureListener { Timber.e(it, "read error %s", readOptions) }
     }
 }
